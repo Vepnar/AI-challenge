53f752b38436cc6f836a31fd6aeb58af44b7efd3
person <agdehaan@gmail.com>
2020-06-23 17:16:00 +0200

    bucket-downloader can download more images now

--- bucket-downloader.sh
+++ bucket-downloader.sh
@@ -1,16 +1,9 @@
-# Download from 1 to 200 to get all samples with 0 items 
-# Download from 201 to 400 to get all samples with 1 item
-# Download from 401 to 600 to get all samples with 2 items
-# Download form 601 to 800 to get all samples with 3 items
-# 801 to 1000 for 4 items
-# 1001 to 1200 for 5 items
-
-start=1
-end=1200
+start=11467
+end=200000
 image_dir="./dataset/images"
 metadata_dir="./dataset/metadata"
 
-for i in $(seq $start $end); do
+for i in $(seq -f '%05g' $start $end); do
     wget https://aft-vbi-pds.s3.amazonaws.com/metadata/$i.json -P $metadata_dir &
     wget https://aft-vbi-pds.s3.amazonaws.com/bin-images/$i.jpg -P $image_dir
 done
