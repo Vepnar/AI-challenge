704f6201f9e73c289f556b033da6b0af5fb058e0
person <agdehaan@gmail.com>
2020-06-23 12:24:08 +0200

    Added image procesor & updated readme.md

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 I'm trying to solve this challenge [this]("https://github.com/silverbottlep/abid_challenge") without an amazon AWS account.
 
-# First problem
-I do not have an Amazon AWS account and I can not create one because it requires credit card information which I don't have. That's why I came up with an alternative.
+## First problem
+I do not have an Amazon AWS account and I can not create one because it requires credit card information which I dont have. So I came up with an alternative.
 [this script]("https://github.com/Vepnar/AI-challenge/blob/master/bucket-downloader.sh") Can download all files and metadata from the amazon bucket to my computer without the need of an amazon bucket
+
+## Second thing to do.
+We need to preprocess our images to be the same size with OpenCV2.
+
+I used `image_processor.py` to make all images 50x50 pixels and made them grayscale.
+
+This will reduce the amount of computing power I need to train the model.
+
+# How to run this on your system
+1. Clone my github repository on your system.
+`git clone https://github.com/Vepnar/AI-challenge.git`
+2. Download all requirements
+`sudo apt-get install wget python3 python3-opencv2`
+3. CD into into my github repository
+`cd AI-challenge`
+3. Make the bash file executable
+`chmod +x bucket-downloader.sh`
+4. Download all images (This might take a while)
+`./bucket-downloader.sh`
+5. Process all downloaded images
+`python3 image_processor.py`
+
---
+++ image_processor.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import os
+import cv2
+import json
+import random
+import numpy as np
+import tensorflow as tf
+
+# The smaller the images the faster the neuralnetwork learns.
+IMG_HEIGHT = 50
+# The deeplearning neuralnetwork always need images from the same size.
+IMG_WIDTH = 50
+
+# The directory where we downloaded our images.
+DATASET_IMAGES = "./dataset/images"
+DATASET_METADATA = "./dataset/metadata"
+
+DATASET_LABELS = "./dataset/labels.npy"
+DATASET_FEATURES = "./dataset/features.npy"
+
+
+def process_image(path):
+
+    # Grayscale the images will greatly reduce the size of the data.
+    img_array = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+    # Resizing the images will also reduce the size of the image (makes sense).
+    # Plus not all images are equal size and our neural net will require images of equal size.
+    img_array = cv2.resize(img_array, (IMG_WIDTH, IMG_HEIGHT))
+
+    # Parse the filename to find the metadata file.
+    file_base = os.path.basename(path)
+    file_name = os.path.splitext(file_base)[0]
+    metadata_path = f"{DATASET_METADATA}/{file_name}.json"
+
+    # Open the meta.
+    with open(metadata_path) as metadata_file:
+        metadata = json.load(metadata_file)
+        expected_quantity = metadata["EXPECTED_QUANTITY"]
+
+    # Return processed image and the expected quantity.
+    return img_array, expected_quantity
+
+
+if __name__ == "__main__":
+
+    # Store all input data and labels in one list.
+    training_data = []
+
+    # Go through all images listed in the dataset directory
+    print("Resizing and recolouring images")
+    for img in os.listdir(DATASET_IMAGES):
+        real_path = os.path.join(DATASET_IMAGES, img)
+
+        # Process our images.
+        x, y = process_image(real_path)
+        training_data.append((x, y))
+
+    # Shuffle the images.
+    random.shuffle(training_data)
+
+    # Features
+    x = []
+    # Labels
+    y = []
+
+    # Add our shuffeld data to the feature & label array.
+    for features, labels in training_data:
+        x.append(features)
+        y.append(labels)
+
+    # Reshape the features array.
+    x = np.array(x).reshape(-1, IMG_HEIGHT, IMG_WIDTH, 1)
+
+    # Save our procesed data to a numpy file
+    np.save(DATASET_LABELS, x)
+    np.save(DATASET_FEATURES, x)
