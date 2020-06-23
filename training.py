c96c57ac8dda23fa97c0e84b240130fe4cddf158
person <agdehaan@gmail.com>
2020-06-23 20:44:02 +0200

    Make the training.py with functions

--- training.py
+++ training.py
@@ -13,24 +13,11 @@ from tensorflow.keras.layers import (
     MaxPooling2D,
 )
 
-# This is from stackoverflow
-# Source: https://stackoverflow.com/questions/53698035/failed-to-get-convolution-algorithm-this-is-probably-because-cudnn-failed-to-in
-gpus = tf.config.experimental.list_physical_devices("GPU")
-if gpus:
-    # Restrict TensorFlow to only allocate 1*X GB of memory on the first GPU
-    try:
-        tf.config.experimental.set_virtual_device_configuration(
-            gpus[0],
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(784))],
-        )
-        logical_gpus = tf.config.experimental.list_logical_devices("GPU")
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    except RuntimeError as e:
-        # Virtual devices must be set before GPUs have been initialized
-        print(e)
-
 # The smaller the images the faster the neuralnetwork learns.
 IMG_HEIGHT = 50
+
+# Limit gpu usage. zero when there is no limit
+GPU_LIMIT = 784
 
 # The deeplearning neuralnetwork always need images from the same size.
 IMG_WIDTH = 50
@@ -42,50 +29,103 @@ EPOCH = 25
 VALIDATION_SPLIT = 0.1
 
 # Size of training batches
-BATCH_SIZE = 256
+BATCH_SIZE = 200
 
 DATASET_LABELS = "./dataset/labels.npy"
 DATASET_FEATURES = "./dataset/features.npy"
 LOG_DIR = "./logs/{}"
 
-NAME = f"bin-counter-{int(time.time())}"
 
-# Load the features & labels.
-x = numpy.load(DATASET_FEATURES)
-y = numpy.load(DATASET_LABELS)
+def setup_gpu_limiter(vram):
+    """limit the amound of VRAM the gpu could use.
+    This is from stackoverflow
+    Source: https://stackoverflow.com/questions/53698035/failed-to-get-convolution-algorithm-this-is-probably-because-cudnn-failed-to-in
+    """
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if gpus:
+        # Restrict TensorFlow to only allocate 1*X GB of memory on the first GPU
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [
+                    tf.config.experimental.VirtualDeviceConfiguration(
+                        memory_limit=(vram)
+                    )
+                ],
+            )
+            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
 
-# The amount of classes in the dataset.
-CLASSES = len(y[0])
 
-# Load tensorboard
-tensorboard = TensorBoard(log_dir="logs/{NAME}")
+def load_dataset():
+    """Load the features & labels."""
+    x = numpy.load(DATASET_FEATURES)
+    y = numpy.load(DATASET_LABELS)
 
-# Created model wigh layers
-model = Sequential(
-    [
-        Conv2D(64, 3, padding="same", activation="relu", input_shape=x.shape[1:],),
-        MaxPooling2D(),
-        Conv2D(64, 3, padding="same", activation="relu",),
-        MaxPooling2D(),
-        Flatten(),
-        Dense(128, activation="relu"),
-        Dense(CLASSES),
-    ]
-)
+    # The amount of classes in the dataset.
+    classes = len(y[0])
 
-# Compile the model with the adam optimizer and categorial cross entropy.
-model.compile(
-    optimizer="adam",
-    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
+    return x, y, classes
 
-# Train the model
-model.fit(
-    x,
-    y,
-    batch_size=BATCH_SIZE,
-    epochs=EPOCH,
-    validation_split=VALIDATION_SPLIT,
-    callbacks=[tensorboard],
-)
+
+def get_tensorboard(name):
+    return TensorBoard(log_dir=f"logs/{name}")
+
+
+def create_model(x, classes):
+    # Created model wigh layers
+    model = Sequential(
+        [
+            Conv2D(64, 3, padding="same", activation="relu", input_shape=x.shape[1:],),
+            MaxPooling2D(),
+            Conv2D(64, 3, padding="same", activation="relu",),
+            MaxPooling2D(),
+            Flatten(),
+            Dense(128, activation="relu"),
+            Dense(classes),
+        ]
+    )
+
+    # Compile the model with the adam optimizer and categorial cross entropy.
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=["accuracy"],
+    )
+    return model
+
+
+def train_model(model, x, y, tensorboard):
+    # Train the model
+    model.fit(
+        x,
+        y,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCH,
+        validation_split=VALIDATION_SPLIT,
+        callbacks=[tensorboard],
+    )
+
+
+if __name__ == "__main__":
+
+    # Only limit the gpu usage when this is enabled
+    if GPU_LIMIT > 0:
+        setup_gpu_limiter(GPU_LIMIT)
+
+    # Name of the current train session.
+    name = f"Training-{str(time.time())}"
+
+    # Get tensorboard.
+    tensorboard = get_tensorboard(name)
+
+    # Load npy files with the training data.
+    x, y, classes = load_dataset()
+
+    model = create_model(x, classes)
+
+    # Train the model with our labels and features.
+    train_model(model, x, y, tensorboard)
