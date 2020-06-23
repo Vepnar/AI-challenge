2cf46ee302f9ae647db61234cb38d2b25d736120
person <agdehaan@gmail.com>
2020-06-23 19:52:38 +0200

    Implemented tensorboard

--- training.py
+++ training.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import time
 import numpy
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.layers import (
     Dense,
     Dropout,
@@ -13,13 +15,13 @@ from tensorflow.keras.layers import (
 
 # This is from stackoverflow
 # Source: https://stackoverflow.com/questions/53698035/failed-to-get-convolution-algorithm-this-is-probably-because-cudnn-failed-to-in
-# gpus = tf.config.experimental.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices("GPU")
 if gpus:
     # Restrict TensorFlow to only allocate 1*X GB of memory on the first GPU
     try:
         tf.config.experimental.set_virtual_device_configuration(
             gpus[0],
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(1024))],
+            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=(784))],
         )
         logical_gpus = tf.config.experimental.list_logical_devices("GPU")
         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
@@ -42,22 +44,28 @@ VALIDATION_SPLIT = 0.1
 # Size of training batches
 BATCH_SIZE = 256
 
-# The amount of classes
-CLASSES = 3
-
 DATASET_LABELS = "./dataset/labels.npy"
 DATASET_FEATURES = "./dataset/features.npy"
+LOG_DIR = "./logs/{}"
+
+NAME = f"bin-counter-{int(time.time())}"
 
 # Load the features & labels.
 x = numpy.load(DATASET_FEATURES)
 y = numpy.load(DATASET_LABELS)
+
+# The amount of classes in the dataset.
+CLASSES = len(y[0])
+
+# Load tensorboard
+tensorboard = TensorBoard(log_dir="logs/{NAME}")
 
 # Created model wigh layers
 model = Sequential(
     [
         Conv2D(64, 3, padding="same", activation="relu", input_shape=x.shape[1:],),
         MaxPooling2D(),
-        Conv2D(128, 3, padding="same", activation="relu",),
+        Conv2D(64, 3, padding="same", activation="relu",),
         MaxPooling2D(),
         Flatten(),
         Dense(128, activation="relu"),
@@ -73,4 +81,11 @@ model.compile(
 )
 
 # Train the model
-model.fit(x, y, batch_size=BATCH_SIZE, epochs=EPOCH, validation_split=VALIDATION_SPLIT)
+model.fit(
+    x,
+    y,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCH,
+    validation_split=VALIDATION_SPLIT,
+    callbacks=[tensorboard],
+)
